ce2b5f10ee86aa862a382b2f0bbf44a3913082a3
Ax <axdeklerk@gmail.com>
2025-01-19 12:47:57 +0000

    second commit

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -1,7 +1,7 @@
 const addition = require ("../calc");
 
 describe ("calculator", () => {
-    describe ("Addition function", () => {
+    describe ("addition function", () => {
         test ("should return 42 for 20 + 22", () => {
             expect (addition (20,22)). tobe (42);
 
