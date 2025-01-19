cb6529495501e579852be6f22424385e6c855a44
Ax <axdeklerk@gmail.com>
2025-01-19 12:29:34 +0000

    first test

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,23 @@
+const addition = require ("../calc");
+
+describe ("calculator", () => {
+    describe ("Addition function", () => {
+        test ("should return 42 for 20 + 22", () => {
+            expect (addition (20,22)). tobe (42);
+
+        })
+
+    });
+
+    describe ("Subtraction function", () => {
+
+    });
+
+    describe ("Multiply function", () => {
+
+    });
+
+    describe ("Division function", () => {
+
+    });
+})
