260cadee05b287819031246c885daa763c29ed68
Ax <axdeklerk@gmail.com>
2025-01-19 14:14:32 +0000

    new test

--- scripts/tests/calc.test.js
+++ scripts/tests/calc.test.js
@@ -4,7 +4,10 @@ describe ("Calculator", () => {
     describe ("Addition function", () => {
         test ("should return 42 for 20 + 22", () => {
             expect (addition (20,22)). toBe (42);
-        })
+        });
+        test ("should return 73 for 50 + 23", () = > {
+            expect (addition (50 + 23)). toBe (73);
+        });
     });
 
     describe ("Subtraction function", () => {
